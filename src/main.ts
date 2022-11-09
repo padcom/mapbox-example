6e0caf40e5600259f3328adb46c40bda122167e7
Matthias Hryniszak <padcom@gmail.com>
2022-11-09 14:02:21 +0100

    Added option to create random markers

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,9 @@ import mapbox from 'mapbox-gl'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Public
 mapbox.accessToken = 'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg'
+// Development
+//mapbox.accessToken = 'pk.eyJ1IjoicGFkY29tIiwiYSI6ImNsYWJocW9vNTAwa2wzcG1peXQ0NmdtN20ifQ.VKFuSEJ-nXZRKDGIN4tbsw'
 
 createApp(App).mount('#app')
-
-// async function loadImage(map: Map, url: string): Promise<Nullable<HTMLImageElement | ImageBitmap>> {
-//   return new Promise((resolve, reject) => {
-//     map.loadImage(url, (err, image) => {
-//       if (err) reject(err)
-//       else resolve(image || null)
-//     })
-//   })
-// }
