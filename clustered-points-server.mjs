c241e0dfc049e2446c2fac24526c338fb731b967
Matthias Hryniszak <padcom@gmail.com>
2022-11-12 20:39:45 +0100

    Added loading of places from local server

--- clustered-points-server.mjs
+++ clustered-points-server.mjs
@@ -3,10 +3,17 @@
 import Supercluster from 'supercluster'
 import express from 'express'
 import cors from 'cors'
+import compression from 'compression'
 import fetch from 'node-fetch'
 
-const data = await fetch('http://localhost:9070/places.json')
-  .then(response => response.json())
+// Pause for a moment because the FE host needs a bit to start serving files
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+await sleep(500)
+
+const data = await fetch('http://localhost:5173/places.json.gz')
+  .then(response =>
+    response.json()
+    )
   .then(collection => collection.features)
 
 const supercluster = new Supercluster({
@@ -20,6 +27,7 @@ supercluster.load(data)
 
 const app = express()
 app.use(cors())
+
 app.get('/:bbox/:zoom', (req, res) => {
   const bbox = req.params.bbox.split(',').map(parseFloat)
   const zoom = Math.floor(parseFloat(req.params.zoom))
@@ -32,3 +40,6 @@ const listener = app.listen(9000, () => {
 })
 
 // eg: /1,2,3,4/5
+// siege http://localhost:9000/9,47,10,48/10
+
+// app.use(compression())
